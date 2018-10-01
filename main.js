669652a740335af6e5c7bed69ce98aa331efdf89
Ivan <37029650+Vanca93@users.noreply.github.com>
2018-10-01 14:30:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+var slider = tns({
+            container: ".my-slider",
+            items: 4,
+            slideBy: 1,
+            speed: 800,
+            gutter: 20,
+            controls: false,
+            nav: false,
+        });
+let a = document.querySelectorAll('img');
+let title = document.getElementById('title');
+let review = document.getElementById('review');
+let counter = document.getElementById('counter');
+for (let i = 0; i < a.length; i++) {
+    a[i].addEventListener('click', function(e) {
+        let id = parseInt(e.target.src.slice(-6, -5));
+        counter.innerHTML = "0" + id;
+        id = id > 3 ? id - 4 : id + 1;
+        slider.goTo(id);
+        title.innerHTML = a[i].getAttribute("data-title");
+        review.innerHTML = a[i].getAttribute("data-review");
+        for (let i = 0; i < a.length; i++) {
+            a[i].classList.remove('selected');
+            a[i].src === e.target.src && a[i].classList.add('selected');
+        }
+    });
+}
+document.querySelector('.arrow').addEventListener('click', function () {
+    let arr = document.getElementsByClassName('tns-slide-active');
+    slider.goTo('prev');
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].classList.remove('selected');
+    }
+    arr[3].classList.add('selected');
+});
